55c2fea87ef030c5a090faade7dc5803f90ea0cf
cspp01 <1015381017@qq.com>
2017-01-11 18:55:28 +0800

    gulp

---
+++ dist/js/index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Administrator on 2017/1/11/011.
+ */
+$(function(){
+    alert("2");
+});
+/**
+ * Created by Administrator on 2017/1/11/011.
+ */
+$(function(){
+    alert("1");
+});
---
+++ gulpfile.js
@@ -0,0 +1,118 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var livereload = require('gulp-livereload');
+var rename = require('gulp-rename');
+var minifyCss = require('gulp-minify-css');
+var minifyHtml = require('gulp-minify-html');
+var clean = require('gulp-clean');
+var zip = require('gulp-zip');
+var minifyJs = require('gulp-uglify');
+var concat = require('gulp-concat');
+var jshint = require('gulp-jshint');
+var plumber = require('gulp-plumber');
+var imagemin = require('gulp-imagemin');
+var autoprefixer = require('gulp-autoprefixer');
+var pngquant = require('imagemin-pngquant');
+var cache = require('gulp-cache');
+var notify = require('gulp-notify');
+/*css编译*/
+gulp.task('less', function() {
+        return gulp.src('src/less/*.less')
+            .pipe(plumber())
+        .pipe(less())
+            .pipe(autoprefixer('last 2 versions', 'last 1 Chrome versions', 'last 2 Explorer versions', 'last 3 Safari versions', 'Firefox >= 20'))
+            .pipe(concat('index.css'))
+            .pipe(gulp.dest('dist/css'))
+            .pipe(notify({
+                message:'css编译完成'
+            }));
+    });
+/*css压缩*/
+gulp.task('minifyCss',['less'],function(){
+    return gulp.src('dist/css/index.css')
+    .pipe(minifyCss())
+        .pipe(rename('index.min.css'))
+    .pipe(gulp.dest('dist/css'))
+        .pipe(notify({
+            message:'css压缩完成'
+        }))
+    .pipe(livereload());
+
+});
+/*html压缩*/
+gulp.task('minifyHtml',function(){
+    return gulp.src('src/html/**/*.html')
+        .pipe(minifyHtml())
+        .pipe(gulp.dest('dist/html'))
+        .pipe(livereload())
+        .pipe(notify({
+            message:'html压缩完成'
+        }))
+        .pipe(livereload());
+});
+/*js代码检查*/
+gulp.task('jshint',function(){
+    return gulp.src('src/js/*.js')
+        .pipe(plumber())
+        .pipe(jshint())
+        .pipe(jshint.reporter());
+});
+/*合并js*/
+gulp.task('concatJs',['jshint'],function(){
+    return gulp.src('src/js/*.js')
+        .pipe(concat('index.js'))
+        .pipe(gulp.dest('dist/js'))
+        .pipe(notify({
+            message:'合并js完成'
+        }));
+});
+/*压缩js*/
+gulp.task('minifyJs',['concatJs'],function(){
+    return gulp.src('dist/js/index.js')
+        .pipe(minifyJs())
+        .pipe(rename('index.min.js'))
+        .pipe(gulp.dest('dist/js'))
+        .pipe(notify({
+            message:'压缩js完成'
+        }))
+        .pipe(livereload());
+});
+/*压缩图片*/
+gulp.task('imagemin',function(){
+    return gulp.src('src/image/*.jpg')
+        .pipe(cache(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        })))
+        .pipe(gulp.dest('dist/image'))
+        .pipe(notify({
+            message:'压缩图片完成'
+        }))
+        .pipe(livereload());
+});
+/*打包zip*/
+gulp.task('zip',function(){
+    return gulp.src('dist/**')
+        .pipe(zip('jd.zip'))
+        .pipe(gulp.dest(''))
+        .pipe(notify({
+            message:'打包zip完成'
+        }));
+});
+/*删除打包zip*/
+gulp.task('clean',function(){
+    return gulp.src('jd.zip')
+        .pipe(clean());
+});
+gulp.task('watch', function () {
+        livereload.listen();
+       gulp.watch(['src/less/*.less'], ['minifyCss']);/*css编译压缩*/
+    gulp.watch(['src/html/**/*.html'], ['minifyHtml']);/*html压缩*/
+    gulp.watch(['src/js/**/*.js'], ['minifyJs']);/*js压缩*/
+    //gulp.watch(['src/image/*.*'], ['imagemin']);/*j图片压缩*/
+     });
+/*默认任务*/
+gulp.task('default',['clean'],function(){
+    gulp.run('minifyCss','minifyJs'/*,'imagemin'*/,'minifyHtml','zip','watch');
+});
